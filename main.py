dae2ed07aeb38f886709060f2709c79f21ea2140
Bartosz Kościów <kosci1@gmail.com>
2017-03-15 19:38:54 +0100

    drawing  lines

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ LED = 6
 GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, 1)
 
-
 s = TFT()
 s.init()
 
 for i in range(0, 11):
     s.draw_pixel(100 + i, 100)
     s.draw_pixel(160 + i, 100)
-
 
 s.color = {'R': 128, 'G': 128, 'B': 128}
 
