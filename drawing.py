8c86f9ad54ef6997e9cb49adad4f479927bffbf7
Bartosz Kościów <kosci1@gmail.com>
2017-04-16 22:56:36 +0200

    drawing a line

--- drawing.py
+++ drawing.py
@@ -22,6 +22,15 @@ def draw_points(o):
     for _ in range(0, 50):
         hole(o, random.randint(2,o.width - 10), random.randint(2,o.height-10))
 
+def draw_net(o):
+    s = 0
+    while s < o.width-1:
+        o.draw_line(s, 0, s, o.height-1)
+        s += 10
+    s = 0
+    while s < o.height-1:
+        o.draw_line(0, s, o.width-1, s)
+        s += 10
 
 lcd_oled = SSD1306(128, 64, SPI())
 lcd_oled.init()
@@ -31,9 +40,19 @@ lcd_nju = NJU6450(122, 32, GPIO())
 lcd_nju.init()
 lcd_nju.auto_flush = False
 
-lcd_oled.draw_line(0, 0, 100, 20)
+lcd_oled.draw_line(0, 0, lcd_oled.width-1, lcd_oled.height-1)
+lcd_oled.draw_line(0, lcd_oled.height-1, lcd_oled.width-1, 0)
+
+draw_net(lcd_oled)
+draw_net(lcd_nju)
+
+lcd_oled.draw_line(0, 0, lcd_oled.width-1, 0)
+lcd_oled.draw_line(0, lcd_oled.height-1, lcd_oled.width-1, lcd_oled.height-1)
 #draw_points(lcd_oled)
 #draw_points(lcd_nju)
+
+lcd_nju.draw_line(0, 0, lcd_nju.width-1, lcd_nju.height-1)
+lcd_nju.draw_line(0, lcd_nju.height-1, lcd_nju.width-1, 0)
 
 lcd_oled.flush(True)
 lcd_nju.flush(True)
