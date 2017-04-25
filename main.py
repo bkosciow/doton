d4fe0c937ae0356404a66df2f328d9b2b471cf5c
Bartosz Kościów <kosci1@gmail.com>
2017-04-25 22:09:28 +0200

    refacor main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
-from screen import TFT
+from gfxlcd.driver.ili9325.gpio import GPIO as ILIGPIO
+from gfxlcd.driver.ili9325.ili9325 import ILI9325
 import time
 GPIO.setmode(GPIO.BCM)
 
@@ -8,66 +9,28 @@ LED = 6
 GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, 1)
 
-s = TFT()
-s.init()
+lcd_tft = ILI9325(240, 320, ILIGPIO())
+lcd_tft.init()
 
-s.bcolor = {'R': 255, 'G': 0, 'B': 0}
-s.fill_rect(10, 10, 50, 50)
+lcd_tft.background_color = (255, 0, 0)
+lcd_tft.fill_rect(10, 10, 50, 50)
 
-s.bcolor = {'R': 0, 'G': 255, 'B': 0}
-s.fill_rect(230, 50, 190, 10)
+lcd_tft.background_color = (0, 255, 0)
+lcd_tft.fill_rect(230, 50, 190, 10)
 
-s.bcolor = {'R': 0, 'G': 0, 'B': 255}
-s.fill_rect(230, 310, 190, 270)
+lcd_tft.background_color = (0, 0, 255)
+lcd_tft.fill_rect(230, 310, 190, 270)
 
-s.bcolor = {'R': 255, 'G': 255, 'B': 0}
-s.fill_rect(10, 310, 50, 270)
+lcd_tft.background_color = (255, 255, 0)
+lcd_tft.fill_rect(10, 310, 50, 270)
 
-s.draw_circle(120, 160, 100)
-s.draw_circle(70, 120, 20)
-s.draw_circle(170, 120, 20)
+lcd_tft.draw_circle(120, 160, 100)
+lcd_tft.draw_circle(70, 120, 20)
+lcd_tft.draw_circle(170, 120, 20)
 
-s.bcolor = {'R': 250, 'G': 0, 'B': 255}
-s.draw_arc(120, 160, 70, 20, 160)
+lcd_tft.background_color = (250, 0, 255)
+lcd_tft.draw_arc(120, 160, 70, 20, 160)
 
-s.draw_line(120, 150, 115, 180)
-s.draw_line(120, 150, 125, 180)
-s.draw_line(115, 180, 125, 180)
-
-# for i in range(0, 11):
-#     s.draw_pixel(100 + i, 100)
-#     s.draw_pixel(160 + i, 100)
-#
-#
-# s.color = {'R': 128, 'G': 128, 'B': 128}
-#
-# for i in range(1, 11):
-#     s.draw_line(0, i * 30, 239, i * 30)
-#
-# for i in range(1, 9):
-#     s.draw_line(i * 30, 0, i * 30, 319)
-#
-#
-# s.color = {'R': 255, 'G': 0, 'B': 0}
-# s.draw_line(60, 60, 47, 202)
-# s.color = {'R': 0, 'G': 255, 'B': 0}
-# s.draw_line(60, 60, 73, 202)
-#
-# s.color = {'R': 0, 'G': 0, 'B': 255}
-# s.draw_line(180, 240, 167, 88)
-# s.color = {'R': 255, 'G': 255, 'B': 0}
-# s.draw_line(180, 240, 193, 88)
-#
-#
-# s.color = {'R': 255, 'G': 0, 'B': 0}
-# s.draw_line(60, 50, 150, 57)
-# s.color = {'R': 0, 'G': 255, 'B': 0}
-# s.draw_line(150, 43, 60, 50)
-#
-# s.color = {'R': 0, 'G': 0, 'B': 255}
-# s.draw_line(60, 270, 182, 241)
-# s.color = {'R': 255, 'G': 255, 'B': 0}
-# s.draw_line(182, 292, 60, 270)
-#
-# s.color = {'R': 255, 'G': 255, 'B': 255}
-# s.draw_rect(30,30, 210, 290)
+lcd_tft.draw_line(120, 150, 115, 180)
+lcd_tft.draw_line(120, 150, 125, 180)
+lcd_tft.draw_line(115, 180, 125, 180)
