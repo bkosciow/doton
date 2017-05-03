47d32fc3c6c8f6d13049593a87f20f29979be514
Bartosz Kościów <kosci1@gmail.com>
2017-05-03 12:43:43 +0200

    add Pages

--- service/window_manager.py
+++ service/window_manager.py
@@ -1,3 +1,4 @@
+"""Window Manager"""
 import threading
 import time
 
@@ -9,11 +10,18 @@ class WidgetHolder(object):
         self.widget = widget
 
 
+class Page(object):
+    """Page class"""
+    def __init__(self):
+        self.widgets = {}
+
+
 class WindowManager(threading.Thread):
     """Window Manager"""
     def __init__(self, lcd):
         threading.Thread.__init__(self)
-        self.widgets = {}
+        self.pages = [Page()]
+        self.active_page = 0
         self.lcd = lcd
         self.work = True
 
@@ -23,15 +31,16 @@ class WindowManager(threading.Thread):
         for coords in coordinates:
             position.append((coords[0]*134, coords[1]*106))
 
-        self.widgets[name] = WidgetHolder(position, widget)
+        self.pages[page].widgets[name] = WidgetHolder(position, widget)
 
     def run(self):
         """main loop - drawing"""
-        for holder in self.widgets:
-            self.widgets[holder].widget.draw_widget(self.lcd, self.widgets[holder].coords)
+        widgets = self.pages[self.active_page].widgets
+        for holder in widgets:
+            widgets[holder].widget.draw_widget(self.lcd, widgets[holder].coords)
         while self.work:
-            for holder in self.widgets:
-                self.widgets[holder].widget.draw_values(self.lcd, self.widgets[holder].coords)
+            for holder in widgets:
+                widgets[holder].widget.draw_values(self.lcd, widgets[holder].coords)
             time.sleep(0.025)
 
     def stop(self):
@@ -40,16 +49,17 @@ class WindowManager(threading.Thread):
 
     def set_widget_color(self, name, key, value):
         """change colour"""
-        self.widgets[name].widget.colours[key] = value
+        self.pages[self.active_page].widgets[name].widget.colours[key] = value
 
     def get_widget(self, name):
         """get widget by name"""
-        return self.widgets[name].widget
+        return self.pages[self.active_page].widgets[name].widget
 
     def get_widgets(self):
         """returns widgets dictionary"""
-        widgets = {}
-        for hanlder in self.widgets:
-            widgets[hanlder] = self.widgets[hanlder].widget
+        widgets = self.pages[self.active_page].widgets
+        return_widgets = {}
+        for hanlder in widgets:
+            return_widgets[hanlder] = widgets[hanlder].widget
 
-        return widgets
+        return return_widgets
